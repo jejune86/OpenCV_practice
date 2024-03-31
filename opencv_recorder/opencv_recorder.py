5bac3036c496257672b446810a7bf4964e181201
자신의 <jeune86@gmail.com>
2024-03-31 20:58:46 +0900

    undo last commit and delete unrelative file

--- opencv_recorder/opencv_recorder.py
+++ opencv_recorder/opencv_recorder.py
@@ -17,12 +17,22 @@ if video.isOpened() :
     
     while 1 :
         valid, img = video.read()
+
+        img_rec = contrast * img + bright
+        img_rec[img_rec < 0] = 0
+        img_rec[img_rec > 255] = 255
+        img_rec = img_rec.astype(np.uint8)
         if not valid :
             break
-        img_scr = img.copy()
+        
+        if reverse :
+            img_rec = cv.flip(img_rec,1)
+
+        img_scr = img_rec.copy()
+        
         if recording :
             cv.circle(img_scr, (15,15), radius = 10, color = (0,0,255), thickness = -1)
-            rec.write(img)
+            rec.write(img_rec)
             
         cv.imshow('recorder', img_scr)
         key = cv.waitKey(wait_msec)
@@ -30,6 +40,19 @@ if video.isOpened() :
             break
         elif key == 32 :
             recording = not recording
+        elif key == ord('r') :
+            reverse = not reverse
+        elif key == ord('1'):
+                contrast -= 0.1
+                if contrast < 0 : contrast = 0
+        elif key == ord('2'):
+                contrast += 0.1
+        elif key == ord('3') :
+                bright -= 1
+                if bright < 0 :
+                    bright = 0
+        elif key == ord('4') :
+            bright += 1
     video.release()
     rec.release()
     cv.destroyAllWindows()
